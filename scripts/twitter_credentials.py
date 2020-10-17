8450606c56872fef438a5c9c281ad4a1f5458d45
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2020-10-17 09:09:36 -0700

    Update twitter_credentials.py

--- scripts/twitter_credentials.py
+++ scripts/twitter_credentials.py
@@ -3,9 +3,9 @@ Ths file contains the twitter credentials from the twitter application
 in the developer console
 """
 
-ACCESS_TOKEN = "2242232880-nTQh9aZ0YoDAWpXpuyskIhF0Pz4F8qIvmM8UXO5"
-ACCESS_TOKEN_SECRET = "JvdVjtkOCeU8sK18nmpf4zFRStDOzKtQUaB8pnpNoPe3q"
-CONSUMER_KEY = "9e5RcUshCeQme09ZanKsqLvNj"
-CONSUMER_SECRET = "UI4D9PVdBvmcpjkkWtQ0HfVspZn74cVFupAAUKWo9P9vunMJ2F"
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # # # please note that the above token has been revoked and wont work now
