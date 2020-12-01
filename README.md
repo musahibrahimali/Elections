e1be0840eabf7548897de18231146468593ad4a8
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2020-12-01 04:54:23 +0000

    adding more data after extraction

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 this is the updated version of the ai for election prediction repo, this time around all data is extracted with one script file
 
 
-NOTE: be sure to change the twitter credentials to valid ones before running this script.
+# NOTE: 
+### be sure to change the twitter credentials to valid ones before running this script.
--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,7 @@
 """
-This script file extract tweets from the timeline of some targeted politician of the major
-political parties of Ghana, it extracts the 1000 tweets at each run. and another 1000 from
-the timelines of the targeted individuals
+    This script file extract tweets from the timeline of some targeted politician of the major
+    political parties of Ghana, it extracts the 1000 tweets at each run. and another 1000 from
+    the timelines of the targeted individuals
 """
 
 import tweepy
@@ -18,187 +18,147 @@ access_secret = api.ACCESS_TOKEN_SECRET
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
-# api = tweepy.API(auth)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# the Script targets any of these keywords in the loop
-terms = [
-    "akuffo addo", "bawumia", "npp", "president of ghana", "vice president of Ghana",
-    "nana addo dankwa akuffo addo", "nana akuffo addo led administration", "sitting government of ghana",
-    "current president of Ghana", "john dramani mahama", "ndc", "ex-president of ghana",
-    "former president og Ghana", "ex president of Ghana", "mahama led administration"
-]
-# insert the keyword here for the extraction to continue
-searchQuery = ''
-retweet_filter = '-filter:retweets'
 
-# append the term to search parameters
-q = searchQuery + retweet_filter
-tweetsPerQry = 100
-fName = 'tweets.txt'
-sinceId = None
+def main():
+    # the Script targets any of these keywords in the loop
+    terms = [
+        "akuffo addo", "bawumia", "npp", "vice president of Ghana", "nana addo dankwa akuffo addo", "4MoreForNana",
+        "sitting government of ghana", "john dramani mahama", "ndc", "former president of Ghana", "JMandJane2020",
+        "mahama led administration", "JohnMahama2020", "NDCmanifesto"
+    ]
 
-max_id = -1
-maxTweets = 1000
+    # insert the keyword here for the extraction to continue
+    retweet_filter = '-filter:retweets'
 
-tweetCount = 0
+    # append the term to search parameters
+    q = retweet_filter
+    tweets_per_qry = 100
+    since_id = None
 
-# Usernames whose tweets we want to gather.
-users = [
-    "NAkufoAddo",
-    "JDMahama",
-    "MBawumia",
-    "NJOAgyemang",
-    "wontumi_1",
-    "officeofJJR",
-]
+    max_id = -1
+    max_tweets = 1000
 
-# giving the user some feed back that the script is running
-print("Downloading tweets from user timelines...")
+    tweet_count = 0
 
-# extract tweets from timeline of targeted politicians of the major political parties
-try:
-    with open('../data/tweets.csv', 'ab') as file:
-        writer = unicodecsv.writer(file, delimiter=',', quotechar='"')
-        # Write header row.
+    # Usernames whose tweets we want to gather.
+    users = [
+        "NAkufoAddo",
+        "JDMahama",
+        "MBawumia",
+        "NJOAgyemang",
+        "wontumi_1",
+        "ndcgreenarmy",
+    ]
 
-        # write the titles for each column
-        writer.writerow([
-            "Tweet Date",
-            "Tweet ID",
-            "Tweet Text",
-            "tweet_source",
-            "tweet_retweet_count",
-            "tweet_favorite_count"
-        ])
+    # giving the user some feed back that the script is running
+    print("Downloading tweets from user timelines...")
 
-        # loop through all the users and extract tweets from their relative timelines
-        for user in users:
-            user_obj = api.get_user(user)
+    # extract tweets from timeline of targeted politicians of the major political parties
+    try:
+        with open('../data/tweets.csv', 'ab') as file:
+            writer = unicodecsv.writer(file, delimiter=',', quotechar='"')
+            # Write header row.
 
-            # Get 1000 most recent tweets for the current user.
-            for tweet in Cursor(api.user_timeline, screen_name=user).items(1000):
-                # Latitude and longitude stored as array of floats within a dictionary.
-                lat = tweet.coordinates['coordinates'][1] if tweet.coordinates is not None else None
-                long = tweet.coordinates['coordinates'][0] if tweet.coordinates is not None else None
-                # If tweet is not in reply to a screen name, it is not a direct reply.
-                direct_reply = True if tweet.in_reply_to_screen_name != "" else False
-                # Retweets start with "RT ..."
-                retweet_status = True if tweet.text[0:3] == "RT " else False
+            # write the titles for each column
+            writer.writerow([
+                "Tweet Date",
+                "Tweet ID",
+                "Tweet Text",
+                "tweet_source",
+                "tweet_retweet_count",
+                "tweet_favorite_count"
+            ])
 
-                # Get info specific to the current tweet of the current user.
-                tweet_text = unidecode(tweet.text)
+            # loop through all the users and extract tweets from their relative timelines
+            for user in users:
 
-                # format the date
-                tweet_date = str(tweet.created_at.year) + "/" + str(tweet.created_at.month) + "/" + str(
-                    tweet.created_at.day)
+                # Get 1000 most recent tweets for the current user.
+                for tweet in Cursor(api.user_timeline, screen_name=user).items(1000):
+                    # Get info specific to the current tweet of the current user.
+                    tweet_text = unidecode(tweet.text)
 
-                tweet_source = tweet.source
+                    # format the date
+                    tweet_date = str(tweet.created_at.year) + "/" + str(tweet.created_at.month) + "/" + str(
+                        tweet.created_at.day)
 
-                retweet_count = tweet.retweet_count
+                    tweet_source = tweet.source
 
-                favorite_count = tweet.favorite_count
+                    retweet_count = tweet.retweet_count
 
-                tweet_id = tweet.id
+                    favorite_count = tweet.favorite_count
 
-                # Below entities are stored as variable-length dictionaries, if present.
-                hashtags = []
-                hashtags_data = tweet.entities.get('hashtags', None)
-                if hashtags_data is not None:
-                    for i in range(len(hashtags_data)):
-                        hashtags.append(unidecode(hashtags_data[i]['text']))
+                    tweet_id = tweet.id
 
-                # get all urls of a tweet
-                urls = []
-                urls_data = tweet.entities.get('urls', None)
-                if urls_data is not None:
-                    for i in range(len(urls_data)):
-                        urls.append(unidecode(urls_data[i]['url']))
+                    # Write the extracted tweets to CSV.
+                    writer.writerow([
+                        tweet_date, tweet_id, tweet_text, tweet_source,
+                        retweet_count, favorite_count,
+                    ])
 
-                # get all mentions
-                user_mentions = []
-                user_mentions_data = tweet.entities.get('user_mentions', None)
-                if user_mentions_data is not None:
-                    for i in range(len(user_mentions_data)):
-                        user_mentions.append(unidecode(user_mentions_data[i]['screen_name']))
+                # Show progress.
+                print("Wrote tweets of %s to CSV." % user)
 
-                # get all media types
-                media = []
-                media_data = tweet.entities.get('media', None)
-                if media_data is not None:
-                    for i in range(len(media_data)):
-                        media.append(unidecode(media_data[i]['type']))
+            print("streaming at least {0} tweets".format(max_tweets))
+            for term in terms:
+                search_query = term + q
+                while tweet_count < max_tweets:
+                    try:
+                        if max_id <= 0:
+                            if not since_id:
+                                new_tweets = api.search(q=search_query, lang="en", count=tweets_per_qry,
+                                                        tweet_mode='extended')
 
-                # get all contributors
-                contributors = []
-                if tweet.contributors is not None:
-                    for contributor in tweet.contributors:
-                        contributors.append(unidecode(contributor['screen_name']))
-
-                # Write the extracted tweets to CSV.
-                writer.writerow([
-                    tweet_date, tweet_id, tweet_text, tweet_source,
-                    retweet_count, favorite_count,
-                ])
-
-            # Show progress.
-            print("Wrote tweets of %s to CSV." % user)
-
-        print("streaming at least {0} tweets".format(maxTweets))
-        for term in terms:
-            searchQuery = term
-            while tweetCount < maxTweets:
-                tweets = []
-                try:
-                    if max_id <= 0:
-                        if not sinceId:
-                            new_tweets = api.search(q=q, lang="en", count=tweetsPerQry, tweet_mode='extended')
-
+                            else:
+                                new_tweets = api.search(q=search_query, lang="en", count=tweets_per_qry,
+                                                        since_id=since_id, tweet_mode='extended')
                         else:
-                            new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                                    since_id=sinceId, tweet_mode='extended')
-                    else:
-                        if not sinceId:
-                            new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                                    max_id=str(max_id - 1), tweet_mode='extended')
-                        else:
-                            new_tweets = api.search(q=q, lang="en", count=tweetsPerQry,
-                                                    max_id=str(max_id - 1),
-                                                    since_id=sinceId, tweet_mode='extended')
+                            if not since_id:
+                                new_tweets = api.search(q=search_query, lang="en", count=tweets_per_qry,
+                                                        max_id=str(max_id - 1), tweet_mode='extended')
+                            else:
+                                new_tweets = api.search(q=search_query, lang="en", count=tweets_per_qry,
+                                                        max_id=str(max_id - 1),
+                                                        since_id=since_id, tweet_mode='extended')
 
-                    if not new_tweets:
-                        print("No more tweets found")
+                        if not new_tweets:
+                            print("No more tweets found")
+                            break
+                        for tweet in new_tweets:
+                            tweet_text = unidecode(tweet.full_text)
+
+                            tweet_date = str(tweet.created_at.year) + "/" + str(tweet.created_at.month) + "/" + str(
+                                tweet.created_at.day)
+
+                            tweet_source = tweet.source
+
+                            retweet_count = tweet.retweet_count
+
+                            favorite_count = tweet.favorite_count
+
+                            tweet_id = tweet.id
+                            writer.writerow([
+                                tweet_date, tweet_id, tweet_text, tweet_source,
+                                retweet_count, favorite_count,
+                            ])
+
+                        tweet_count += len(new_tweets)
+                        print("Downloaded {0} tweets".format(tweet_count))
+                        max_id = new_tweets[-1].id
+
+                    except tweepy.TweepError as e:
+                        # Just exit if any error
+                        print("some error : " + str(e))
                         break
-                    for tweet in new_tweets:
-                        tweet_text = unidecode(tweet.full_text)
+            print("Downloaded at least {0} tweets, Saved to csv file".format(tweet_count))
+    except tweepy.TweepError as e:
+        print("There was an error, find details below, else check your internet connection or your " +
+              " credentials in the credentials.py file \n")
+        print("If this is not your first time running this particular script, then there is a possibility that the "
+              "maximum rate limit has been exceeded. wait a few more minutes and re run the script.\n")
+        print(f"Error Details: {str(e)}")
 
-                        tweet_date = str(tweet.created_at.year) + "/" + str(tweet.created_at.month) + "/" + str(
-                            tweet.created_at.day)
 
-                        tweet_source = tweet.source
-
-                        retweet_count = tweet.retweet_count
-
-                        favorite_count = tweet.favorite_count
-
-                        tweet_id = tweet.id
-                        writer.writerow([
-                            tweet_date, tweet_id, tweet_text, tweet_source,
-                            retweet_count, favorite_count,
-                        ])
-
-                    tweetCount += len(new_tweets)
-                    print("Downloaded {0} tweets".format(tweetCount))
-                    max_id = new_tweets[-1].id
-
-                except tweepy.TweepError as e:
-                    # Just exit if any error
-                    print("some error : " + str(e))
-                    break
-        print("Downloaded at least {0} tweets, Saved to csv file".format(tweetCount))
-except tweepy.TweepError as e:
-    print("There was an error, find details below, else check your internet connection or your " +
-          " credentials in the credentials.py file \n")
-    print("If this is not your first time running this particular script, then there is a possibility that the "
-          "maximum rate limit has been exceeded. wait a few more minutes and re run the script\n")
-    print(f"Error Details: {str(e)}")
+if __name__ == "__main__":
+    main()
